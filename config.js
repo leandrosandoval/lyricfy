6b8c7615fd17df5c006d66afaec07d977fe82cf6
lsandoval <leandro.77.sandoval@gmail.com>
2018-08-28 22:52:50 -0300

    env added

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {};
+
+config.apiToken='';
+config.appId='ACGA1GWUQ';
+config.clientId='104888213495.424341574976';
+config.clientSecret='e16a1667d40d5a109f89370b2d7883d3';
+config.signintSecret='24c65e5f61864ea7334d74021c10099f';
+
+config.web.port= process.env.PORT || 3894;
+
+module.exports = config;
