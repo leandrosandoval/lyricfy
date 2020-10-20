7b268bd8e39687f2be0266b10d924cbd60c621bd
lsandoval <leandro.77.sandoval@gmail.com>
2020-10-19 17:35:11 -0700

    Adding some stuff

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-require('dotenv').config();
-var express = require('express');
-var app = express();
-var request = require('request');
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+// Require the Bolt package (github.com/slackapi/bolt)
+const { App } = require("@slack/bolt");
+const { lyricsService} = require("songNameFinder")
 
-var port = process.env.PORT
-var apiToken = process.env.API_TOKEN
-var clientId = process.env.CLIENT_ID
-var clientSecret = process.env.CLIENT_SECRET
-var signintSecret = process.env.CLIENT_SIGNING_SECRET
-app.listen(port);
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
 
-console.log('PagChomp is this real life');
+
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
---
+++ service/lyrics-service.js
@@ -0,0 +1,14 @@
+// Song finder.
+
+const songNameFinder = async(lyrics) => {
+    try {
+        // implement finder
+    } catch(err) {
+        console.log('oops, ${err.message}');
+    }
+    
+}
+
+module.exports = {
+ songNameFinder
+};
